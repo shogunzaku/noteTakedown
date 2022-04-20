1daa971dfa16bc47cf5f90caaff238fb8d82bf26
Shogun <shogunzaku@gmail.com>
2022-04-19 17:12:02 -0700

    created get for notes, api/notes and added post

---
+++ server.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+    const parseNotes = JSON.parse(dataNotes);
+    res.json(parseNotes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+    const parseNotes = JSON.parse(dataNotes);
+    parseNotes.push(req.body);
+
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parseNotes), "utf-8");
+    res.json("You added a note!");
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+}); 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './notes.html'));
+});
